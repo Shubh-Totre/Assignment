ea996922cab78c36ebb20d2cdb7e2f7e3d79cc17
shubham <totreshubham2@gmail.com>
2023-05-22 18:08:14 +0530

    First task

---
+++ Task/Models/UpdateEmployee.cs
@@ -0,0 +1,10 @@
+﻿namespace Task.Models
+{
+    public class UpdateEmployee
+    {
+        public string? Name { get; set; }
+        public string? Email { get; set; }
+
+        public string? Role { get; set; }
+    }
+}
